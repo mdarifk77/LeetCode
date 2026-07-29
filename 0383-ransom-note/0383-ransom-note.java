7e3ca4027d6f27fa266d4fefdc5238ad4b1ce70a
Md Arif Karim <mdarifkarim360@gmail.com>
2026-07-29 17:37:06 +0530

    Time: 3 ms (50.91%), Space: 46.2 MB (89.86%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) { 
+
+        int [] count = new int [26];
+
+        for(int i = 0; i < magazine.length(); i++){
+            char ch = magazine.charAt(i);
+            count[ch - 'a']++;
+        }
+        for(int i = 0; i < ransomNote.length(); i++){
+            char ch = ransomNote.charAt(i);
+            count [ ch - 'a']--;
+
+            if(count[ch - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
