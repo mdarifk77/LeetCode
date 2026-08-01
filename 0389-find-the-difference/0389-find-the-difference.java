59fcefd55c5ebc66d1e058c0630e38b37f0a0898
Md Arif Karim <mdarifkarim360@gmail.com>
2026-08-01 17:23:06 +0530

    Time: 4 ms (21.22%), Space: 43 MB (85.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,15 +1,32 @@
 class Solution {
     public char findTheDifference(String s, String t) {
 
-        // XOR APPROACH:
-        char ans = 0;
+        // // XOR APPROACH :
+
+        // char ans = 0;
+
+        // for(int i = 0; i < s.length(); i++){
+        //     ans ^= s.charAt(i);
+        // }
+        // for(int i = 0; i < t.length(); i++){
+        //     ans ^= t.charAt(i);
+        // }
+        // return ans;  
+
+        // COUNT ARRAY : 
+
+        int [] count = new int [26];
 
         for(int i = 0; i < s.length(); i++){
-            ans ^= s.charAt(i);
+            count[s.charAt(i) - 'a']++;
         }
         for(int i = 0; i < t.length(); i++){
-            ans ^= t.charAt(i);
+            count[t.charAt(i) - 'a']--;
+
+            if(count[t.charAt(i) - 'a'] < 0){
+                return t.charAt(i);
+            }
         }
-        return ans;   
+        return ' ';
     }
-}
+}   
