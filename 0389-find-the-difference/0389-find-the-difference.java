543831c51e9300466e39e74e44beb51f271a7495
Md Arif Karim <mdarifkarim360@gmail.com>
2026-08-01 17:24:28 +0530

    Time: 2 ms (66.36%), Space: 42.8 MB (96.96%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,32 +1,32 @@
 class Solution {
     public char findTheDifference(String s, String t) {
 
-        // // XOR APPROACH :
+        // XOR APPROACH :
 
-        // char ans = 0;
+        char ans = 0;
 
-        // for(int i = 0; i < s.length(); i++){
-        //     ans ^= s.charAt(i);
-        // }
-        // for(int i = 0; i < t.length(); i++){
-        //     ans ^= t.charAt(i);
-        // }
-        // return ans;  
+        for(int i = 0; i < s.length(); i++){
+            ans ^= s.charAt(i);
+        }
+        for(int i = 0; i < t.length(); i++){
+            ans ^= t.charAt(i);
+        }
+        return ans;  
 
         // COUNT ARRAY : 
 
-        int [] count = new int [26];
+        // int [] count = new int [26];
 
-        for(int i = 0; i < s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-        }
-        for(int i = 0; i < t.length(); i++){
-            count[t.charAt(i) - 'a']--;
+        // for(int i = 0; i < s.length(); i++){
+        //     count[s.charAt(i) - 'a']++;
+        // }
+        // for(int i = 0; i < t.length(); i++){
+        //     count[t.charAt(i) - 'a']--;
 
-            if(count[t.charAt(i) - 'a'] < 0){
-                return t.charAt(i);
-            }
-        }
-        return ' ';
+        //     if(count[t.charAt(i) - 'a'] < 0){
+        //         return t.charAt(i);
+        //     }
+        // }
+        // return ' ';
     }
 }   
