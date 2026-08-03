5072bf4d177732da094af7f56a6e43d246c44c07
Md Arif Karim <mdarifkarim360@gmail.com>
2026-08-03 22:17:24 +0530

    Time: 1 ms (63.6%), Space: 43.2 MB (36.63%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,16 +1,35 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) { 
 
-        int [] freq = new int [128];
+        //using approach frequency count:
+
+        // int [] freq = new int [128];
+
+        // for(int i = 0; i < jewels.length(); i++){
+        //     char ch = jewels.charAt(i);
+        //     freq[ch]++;
+        // }
+        // int count = 0;
+        // for(int i = 0; i < stones.length(); i++){
+        //     char ch = stones.charAt(i);
+        //     count += freq[ch];
+        // }
+        // return count;
+
+        // using approach HashSet:
+
+        HashSet <Character> set = new HashSet <>();
 
         for(int i = 0; i < jewels.length(); i++){
             char ch = jewels.charAt(i);
-            freq[ch]++;
+            set.add(ch);
         }
         int count = 0;
         for(int i = 0; i < stones.length(); i++){
             char ch = stones.charAt(i);
-            count += freq[ch];
+            if (set.contains(ch)){
+                count++;
+            }
         }
         return count;
         
