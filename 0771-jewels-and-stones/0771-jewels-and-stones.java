a83804eb82c5f3e0878875fb142b61dd82a5ffa3
Md Arif Karim <mdarifkarim360@gmail.com>
2026-08-03 22:18:15 +0530

    Time: 0 ms (100%), Space: 42.9 MB (85.1%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -3,35 +3,35 @@ class Solution {
 
         //using approach frequency count:
 
-        // int [] freq = new int [128];
-
-        // for(int i = 0; i < jewels.length(); i++){
-        //     char ch = jewels.charAt(i);
-        //     freq[ch]++;
-        // }
-        // int count = 0;
-        // for(int i = 0; i < stones.length(); i++){
-        //     char ch = stones.charAt(i);
-        //     count += freq[ch];
-        // }
-        // return count;
-
-        // using approach HashSet:
-
-        HashSet <Character> set = new HashSet <>();
+        int [] freq = new int [128];
 
         for(int i = 0; i < jewels.length(); i++){
             char ch = jewels.charAt(i);
-            set.add(ch);
+            freq[ch]++;
         }
         int count = 0;
         for(int i = 0; i < stones.length(); i++){
             char ch = stones.charAt(i);
-            if (set.contains(ch)){
-                count++;
-            }
+            count += freq[ch];
         }
         return count;
+
+        // using approach HashSet:
+
+        // HashSet <Character> set = new HashSet <>();
+
+        // for(int i = 0; i < jewels.length(); i++){
+        //     char ch = jewels.charAt(i);
+        //     set.add(ch);
+        // }
+        // int count = 0;
+        // for(int i = 0; i < stones.length(); i++){
+        //     char ch = stones.charAt(i);
+        //     if (set.contains(ch)){
+        //         count++;
+        //     }
+        // }
+        // return count;
         
     }
 }
