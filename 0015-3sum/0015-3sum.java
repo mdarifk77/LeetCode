19bcd0de4b3bba10c54cc0f5b9089a98e62348f4
Md Arif Karim <mdarifkarim360@gmail.com>
2026-08-13 19:30:04 +0530

    Time: 33 ms (72.86%), Space: 59 MB (74.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,11 +5,13 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i = 0; i < nums.length - 2; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
+            if(i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
+
             int left = i + 1;
             int right = nums.length - 1;
+
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
 
