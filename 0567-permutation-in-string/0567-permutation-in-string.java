d1c4cc30510e7794131b2a598a2477779b45aba4
Md Arif Karim <mdarifkarim360@gmail.com>
2026-08-20 19:44:06 +0530

    Time: 7 ms (68.76%), Space: 44 MB (73.88%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,47 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
-        if (s1.length() > s2.length()) {
+        if(s1.length() > s2.length()){
             return false;
         }
+        int[] freq1 = new int [26];
+        int[] freq2 = new int [26];
 
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        // Count frequency of s1 and first window of s2
-        for (int i = 0; i < s1.length(); i++) {
+        for(int i = 0; i < s1.length(); i++){
             freq1[s1.charAt(i) - 'a']++;
             freq2[s2.charAt(i) - 'a']++;
         }
-
-        // Check first window
-        if (matches(freq1, freq2)) {
+        if(matches(freq1,freq2)){
             return true;
         }
-
-        // Slide the window
-        for (int i = s1.length(); i < s2.length(); i++) {
-
-            // Add new character
+        for(int i = s1.length(); i < s2.length(); i++){
             freq2[s2.charAt(i) - 'a']++;
-
-            // Remove old character
             freq2[s2.charAt(i - s1.length()) - 'a']--;
 
-            if (matches(freq1, freq2)) {
+            if(matches(freq1,freq2)){
                 return true;
             }
         }
-
         return false;
     }
-
-    private boolean matches(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) {
+    private boolean matches(int[] a, int [] b){
+        for(int i = 0; i < 26; i++){
+            if(a[i] != b[i]){
                 return false;
             }
         }
         return true;
     }
-}
+}    
+
+        
